f5ffc072e4a5f2d7477ea4cc96ddd96c10e1623a
Code CAPS <codingincaps@gmail.com>
2020-11-23 22:45:55 -0500

    Adding the project for the Introduction to Programming with Unity and C# (1) video.

---
+++ Introduction to Programming with Unity and C# (1)/Assets/Thermostat.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Thermostat : MonoBehaviour
+{
+    float CelsiusToFahrenheit(float celsiusTemperature)
+    {
+        return (celsiusTemperature * 9.0f / 5.0f) + 32.0f;
+    }
+
+    float FahrenheitToCelsius(float fahrenheitTemperature)
+    {
+        return (fahrenheitTemperature - 32.0f) * 5.0f / 9.0f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // freezing points
+        float temperature = CelsiusToFahrenheit(0.0f);
+        Debug.Log("0 degrees Celsius is " + temperature.ToString("F0") + " degrees Fahrenheit.");
+
+        temperature = FahrenheitToCelsius(32.0f);
+        Debug.Log("32 degrees Fahrenheit is " + temperature.ToString("F0") + " degrees Celsius.");
+
+        // boiling points
+        temperature = CelsiusToFahrenheit(100.0f);
+        Debug.Log("100 degrees Celsius is " + temperature.ToString("F0") + " degrees Fahrenheit.");
+
+        temperature = FahrenheitToCelsius(212.0f);
+        Debug.Log("212 degrees Fahrenheit is " + temperature.ToString("F0") + " degrees Celsius.");
+
+        // room temperatures
+        temperature = CelsiusToFahrenheit(20.0f);
+        Debug.Log("20 degrees Celsius is " + temperature.ToString("F0") + " degrees Fahrenheit.");
+
+        temperature = FahrenheitToCelsius(68.0f);
+        Debug.Log("68 degrees Fahrenheit is " + temperature.ToString("F0") + " degrees Celsius.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Introduction to Programming with Unity and C# (1)/Assets/VideoDemo.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class VideoDemo : MonoBehaviour
+{
+
+    int f(int x)
+    {
+        // one implementation
+        //int result = x + 3;
+        //return result;
+
+        // another implementation
+        return x + 3;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // this is a similar notation to the mathematical style f(x) = ...
+        string x = "Hello World!";
+        Debug.Log(x);
+
+        x = "The quick brown fox jumps over the lazy dog.";
+        Debug.Log(x);
+
+        x = "A sample warning message!";
+        Debug.LogWarning(x);
+
+        x = "A sample error message!";
+        Debug.LogError(x);
+
+        int result = f(3);
+        Debug.Log("f(3) = " + result);
+
+        result = f(10);
+        Debug.Log("f(10) = " + result);
+
+        result = f(234);
+        Debug.Log("f(234) = " + result);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+
+}
