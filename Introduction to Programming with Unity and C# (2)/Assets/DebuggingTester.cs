2ecd6c8697a305d76dfa8dc1286233190f1d3881
Code CAPS <codingincaps@gmail.com>
2020-12-05 14:10:10 -0500

    Adding project for the second into to programming video.

---
+++ Introduction to Programming with Unity and C# (2)/Assets/DebuggingTester.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class DebuggingTester : MonoBehaviour
+{
+    float MathTester(float x)
+    {
+        float result = 0.0f;
+
+        float y = 2.12f;
+
+        result = result + x;
+        result = result + (x + y) * y;
+
+        return result;
+    }
+
+    string StringTester(float x)
+    {
+        string result = string.Empty;
+
+        result = "The quick brown fox jumps over the lazy dog ";
+        result = result + x.ToString();
+        result = result + " times.";
+
+        return result;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float result = MathTester(2.0f);
+        Debug.Log("MathTester: " + result.ToString());
+
+        string result2 = StringTester(result);
+        Debug.Log("StringTester: " + result2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
