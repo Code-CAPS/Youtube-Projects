d3215403e3c48291b54d8b63caea7b86c3c93877
Code CAPS <codingincaps@gmail.com>
2020-12-15 19:29:08 -0500

    Starting the next video

---
+++ Clicker Idler Tutorial/Assets/Scripts/OnClick.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class OnClick : MonoBehaviour
+{
+    // declare callback delegate
+    public delegate void OnClickDelegate(OnClick onClickScript);
+
+    // declare a member variable of the type of the callback delegate
+    public OnClickDelegate theDelegate = null;
+
+    void OnMouseUp()
+    {
+        Debug.Log("Mouse Click On: " + this.name);
+
+        if (theDelegate != null)
+        {
+            theDelegate(this);
+        }
+    }
+}
---
+++ Clicker Idler Tutorial/Assets/Scripts/PickingExample.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PickingExample : MonoBehaviour
+{
+    public Material materialDeselect = null;
+    public Material materialSelect = null;
+
+    public List<OnClick> clickTargets = new List<OnClick>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Assertions.Assert.IsNotNull(materialDeselect);
+        UnityEngine.Assertions.Assert.IsNotNull(materialSelect);
+
+        // Set the callback for our click targets and
+        // deselect all targets.
+        foreach (var clickTarget in clickTargets)
+        {
+            clickTarget.theDelegate = OnClickDelegateImplementation;
+
+            var renderer = clickTarget.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.material = materialDeselect;
+            }
+        }
+    }
+
+    public void OnClickDelegateImplementation(OnClick onClickScript)
+    {
+        UnityEngine.Assertions.Assert.IsNotNull(onClickScript);
+
+        // deselect everything
+        foreach (var clickTarget in clickTargets)
+        {
+            var rendererClickTarget = clickTarget.GetComponent<Renderer>();
+            if (rendererClickTarget != null)
+            {
+                rendererClickTarget.material = materialDeselect;
+            }
+        }
+
+        var rendererOnClickScript = onClickScript.GetComponent<Renderer>();
+        if (rendererOnClickScript != null)
+        {
+            rendererOnClickScript.material = materialSelect;
+        }
+    }
+}
