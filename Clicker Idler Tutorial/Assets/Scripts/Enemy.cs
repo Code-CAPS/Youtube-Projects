bec03d490ae3b09e0fc52adb7e09fcab06428ee0
Code CAPS <codingincaps@gmail.com>
2020-12-17 14:43:06 -0500

    finishing the scene baking

---
+++ Clicker Idler Tutorial/Assets/Scripts/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float healthMax = 10.0f;
+    public float healthCurrent = 10.0f;
+
+    public void Damage(float damage)
+    {
+        healthCurrent = healthCurrent - damage;
+        healthCurrent = Mathf.Clamp(healthCurrent, 0.0f, healthMax);
+    }
+}
