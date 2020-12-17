4e94c1e01769d8ecf5e6020d55d97a5d510672f9
Code CAPS <codingincaps@gmail.com>
2020-12-17 18:22:31 -0500

    Spawning an enemy.

--- Clicker Idler Tutorial/Assets/Scripts/GameState.cs
+++ Clicker Idler Tutorial/Assets/Scripts/GameState.cs
@@ -6,16 +6,47 @@ public class GameState : MonoBehaviour
     public GameObject spawnPoint = null;
     public List<GameObject> enemyPrefabs = new List<GameObject>();
 
+    Enemy enemyCurrent = null;
+
     // Start is called before the first frame update
     void Start()
     {
         UnityEngine.Assertions.Assert.IsNotNull(spawnPoint);
         UnityEngine.Assertions.Assert.IsTrue(enemyPrefabs.Count > 0);
+
+        SpawnEnemy();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    [ContextMenu("Spawn an enemy")]
+    void SpawnEnemy()
+    {
+        // remove the old enemy if present
+        DestroyEnemy();
+
+        // create a new enemy
+        int seed = Random.Range(0, enemyPrefabs.Count);
+        GameObject enemyPrefab = enemyPrefabs[seed];
+
+        GameObject enemyNew = Instantiate(enemyPrefab, spawnPoint.transform);
+        enemyNew.transform.localPosition = Vector3.zero;
+        enemyNew.transform.localRotation = Quaternion.identity;
+
+        enemyCurrent = enemyNew.GetComponent<Enemy>();
+        UnityEngine.Assertions.Assert.IsNotNull(enemyCurrent);
+    }
+
+    void DestroyEnemy()
+    {
+        if (enemyCurrent != null)
+        {
+            Destroy(enemyCurrent.gameObject);
+        }
+        enemyCurrent = null;
     }
 }
