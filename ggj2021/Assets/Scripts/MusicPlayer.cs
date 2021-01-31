d0fb9882364d487c47c18466178a35b7aea86ce0
Code CAPS <codingincaps@gmail.com>
2021-01-30 19:43:55 -0500

    getting some sample audio going

---
+++ ggj2021/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioSource audioSource = null;
+    public List<AudioClip> audioClips = new List<AudioClip>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Assertions.Assert.IsNotNull(audioSource);
+        UnityEngine.Assertions.Assert.IsTrue(audioClips.Count > 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            this.PlayMusic();
+        }
+    }
+
+    public void PlayMusic()
+    {
+        int seed = Random.Range(0, audioClips.Count);
+        AudioClip audioClip = audioClips[seed];
+
+        audioSource.Stop();
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+}
