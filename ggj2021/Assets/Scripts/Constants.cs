298818a31c429ea187a2f92777b58cc69ffb7dc5
Code CAPS <codingincaps@gmail.com>
2021-01-31 15:51:10 -0500

    Implementing digging feedback

--- ggj2021/Assets/Scripts/Constants.cs
+++ ggj2021/Assets/Scripts/Constants.cs
@@ -3,4 +3,9 @@
     public static string SCENE_MAIN = "Main";
 
     public static string TWEEN_MAIN_CAMERA = "TMC";
+    public static string SHAKE_PUNCH_POSITION = "SPP";
+    public static string SHAKE_PUNCH_ROTATION = "SPR";
+    public static string SHAKE_PUNCH_SCALE = "SPS";
+    public static string TWEEN_SUCCESS_POSITION = "TSP";
+    public static string TWEEN_SUCCESS_ROTATION = "TSR";
 }
--- ggj2021/Assets/Scripts/WorldController.cs
+++ ggj2021/Assets/Scripts/WorldController.cs
@@ -1,11 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class WorldController : MonoBehaviour
 {
+    public float shakeTime = 0.15f;
+    public float shakeDeltaPosition = 0.1f;
+    public float shakeDeltaRotation = 8.0f;
+    public float shakeDeltaScale = 0.05f;
+
+    public float tweenSuccessTime = 1.0f;
+    public Vector3 tweenSuccessPositionDelta = new Vector3(0.0f, 1.0f, 0.0f);
+    public Vector3 tweenSuccessRotation = new Vector3(0.0f, 360.0f, 0.0f);
+
     public Ground ground = null;
     public Player player = null;
 
     private Diggable diggable = null;
+    private bool animatedSuccess = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,18 +30,13 @@ public class WorldController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // todo idling
-        if (diggable.value <= 0.0f)
-        {
-            Destroy(diggable.gameObject);
-            diggable = null;
 
-            this.SpawnDiggable();
-        }
     }
 
     public void SpawnDiggable()
     {
+        this.animatedSuccess = false;
+
         if (diggable != null)
         {
             Destroy(diggable.gameObject);
@@ -50,15 +56,84 @@ public class WorldController : MonoBehaviour
     // player click delegate
     public void OnClickDelegate(OnClick onClickScript)
     {
-        diggable.value = diggable.value - player.clickStrength;
-        diggable.value = Mathf.Clamp(diggable.value, 0.0f, float.MaxValue);
-
-        if (diggable.value <= 0.0f)
+        if (this.animatedSuccess)
         {
             Destroy(diggable.gameObject);
             diggable = null;
 
             this.SpawnDiggable();
         }
+        else
+        {
+            iTween.StopByName(Constants.SHAKE_PUNCH_POSITION);
+            iTween.StopByName(Constants.SHAKE_PUNCH_ROTATION);
+            iTween.StopByName(Constants.SHAKE_PUNCH_SCALE);
+
+            Vector3 deltaPosition = new Vector3(shakeDeltaPosition, shakeDeltaPosition, shakeDeltaPosition);
+            iTween.ShakePosition(diggable.gameObject, iTween.Hash(
+                "name", Constants.SHAKE_PUNCH_POSITION,
+                "amount", deltaPosition,
+                "time", shakeTime
+                ));
+
+            Vector3 deltaRotation = new Vector3(shakeDeltaRotation, shakeDeltaRotation, shakeDeltaRotation);
+            iTween.ShakeRotation(diggable.gameObject, iTween.Hash(
+                "name", Constants.SHAKE_PUNCH_ROTATION,
+                "amount", deltaRotation,
+                "time", shakeTime
+                ));
+
+            Vector3 deltaScale = new Vector3(shakeDeltaScale, shakeDeltaScale, shakeDeltaScale);
+            iTween.ShakeScale(diggable.gameObject, iTween.Hash(
+                "name", Constants.SHAKE_PUNCH_SCALE,
+                "amount", deltaScale,
+                "time", shakeTime
+                ));
+
+            diggable.value = diggable.value - player.clickStrength;
+            diggable.value = Mathf.Clamp(diggable.value, 0.0f, float.MaxValue);
+
+            if (diggable.value <= 0.0f)
+            {
+                var collider = diggable.GetComponent<Collider>();
+                collider.enabled = false;
+
+                StartCoroutine("AnimateSuccessfulDig");
+            }
+        }
+    }
+
+    public IEnumerator AnimateSuccessfulDig()
+    {
+        iTween.StopByName(Constants.TWEEN_SUCCESS_POSITION);
+        iTween.StopByName(Constants.TWEEN_SUCCESS_ROTATION);
+
+        Vector3 position = this.diggable.transform.position + this.tweenSuccessPositionDelta;
+        iTween.MoveTo(this.diggable.gameObject, iTween.Hash(
+            "name", Constants.TWEEN_SUCCESS_POSITION,
+            "position", position,
+            "time", tweenSuccessTime,
+            "easetype", iTween.EaseType.easeOutCubic,
+            "looptype", iTween.LoopType.none
+            ));
+
+        iTween.RotateAdd(this.diggable.gameObject, iTween.Hash(
+            "name", Constants.TWEEN_SUCCESS_ROTATION,
+            "amount", this.tweenSuccessRotation,
+            "time", tweenSuccessTime,
+            "easetype", iTween.EaseType.easeOutCubic,
+            "looptype", iTween.LoopType.none
+            ));
+
+        yield return new WaitForSeconds(this.tweenSuccessTime);
+
+        this.animatedSuccess = true;
+
+        yield return null;
+
+        var collider = diggable.GetComponent<Collider>();
+        collider.enabled = true;
+
+        yield return null;
     }
 }
